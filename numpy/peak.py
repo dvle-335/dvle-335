9243195368e2d6ff1887cd98bfb45a54395cdb8b
dvle-335 <leducviet1995@gmail.com>
2026-02-15 21:00:04 +0000

    new_file function to load data from file

--- numpy/peak.py
+++ numpy/peak.py
@@ -1,1 +1,12 @@
 import numpy as np
+import pandas as pd
+import sys
+def new_file(name):
+    if name == '':
+        sys.exit('name file not exist')
+    elif name.endwith('.csv' == True):
+        data = pd.read_csv(name)
+    elif name.endwith('.txt' == True):
+        data = pd.read_txt(name)
+    else: 
+        sys.exit('file format not correct')
