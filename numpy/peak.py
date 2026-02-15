335f1c68b5c5de6e675153e36441b161d1ad1e5a
dvle-335 <leducviet1995@gmail.com>
2026-02-15 21:15:14 +0000

    working on new_file function

--- numpy/peak.py
+++ numpy/peak.py
@@ -5,8 +5,9 @@ def new_file(name):
     if name == '':
         sys.exit('name file not exist')
     elif name.endwith('.csv' == True):
-        data = pd.read_csv(name)
+        data = pd.read_csv(name, header = 0).to_numpy()
     elif name.endwith('.txt' == True):
-        data = pd.read_txt(name)
+        data = pd.read_table(name, header = 0).to_numpy()
     else: 
         sys.exit('file format not correct')
+    return data
